717d0990e2341317693b36e394fd8396ba4ff2a7
rakus-yamaguchi <shunya.yamaguchi@rakus-partners.co.jp>
2025-11-13 02:43:18 +0900

    不要ファイルの削除

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
 use App\Presentation\Http\Controllers\Web\HomeController;
 use App\Presentation\Http\Controllers\Web\AboutController;
 use App\Presentation\Http\Controllers\Web\BlogController;
@@ -56,15 +55,3 @@ Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () 
     });
 
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
